7449cbe219ee477d9d2d2e3f6cd554d2f5b653ab
satwik20 <satwik20@users.noreply.github.com>
2014-11-01 16:37:28 -0400

    Create BussinessException.java

---
+++ bms/bms_exception/BussinessException.java
@@ -0,0 +1,12 @@
+package com.cts.bms_exception;
+
+public class BussinessException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	public BussinessException(String msg)
+	{
+	super(msg);
+	}
+	
+}
