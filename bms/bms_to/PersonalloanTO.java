033f6134ca2ac36bb1367e69bb0871ce7debb890
satwik20 <satwik20@users.noreply.github.com>
2014-11-01 16:40:32 -0400

    Create PersonalloanTO.java

---
+++ bms/bms_to/PersonalloanTO.java
@@ -0,0 +1,155 @@
+package com.cts.bms_to;
+
+public class PersonalloanTO {
+
+	/* Variables for Education Loan */
+
+	String loanType;
+	String loanAmount;
+	String applyDate;
+	String issueDate;
+	String rateOfInterest;
+	String durationOfLoan;
+	String loanId;
+	String loanAccountNumber;
+	String loanEmi;
+	String loanStatus;
+	String customerId;
+
+	String income;
+	String name;
+	String design;
+	String texperience;
+	String expwithcompany;
+
+	/* Getters and setters for Education Loan */
+
+	public String getLoanType() {
+		return loanType;
+	}
+
+	public void setLoanType(String loanType) {
+		this.loanType = loanType;
+	}
+
+	public String getLoanAmount() {
+		return loanAmount;
+	}
+
+	public void setLoanAmount(String loanAmount) {
+		this.loanAmount = loanAmount;
+	}
+
+	public String getApplyDate() {
+		return applyDate;
+	}
+
+	public void setApplyDate(String applyDate) {
+		this.applyDate = applyDate;
+	}
+
+	public String getIssueDate() {
+		return issueDate;
+	}
+
+	public void setIssueDate(String issueDate) {
+		this.issueDate = issueDate;
+	}
+
+	public String getRateOfInterest() {
+		return rateOfInterest;
+	}
+
+	public void setRateOfInterest(String rateOfInterest) {
+		this.rateOfInterest = rateOfInterest;
+	}
+
+	public String getDurationOfLoan() {
+		return durationOfLoan;
+	}
+
+	public void setDurationOfLoan(String durationOfLoan) {
+		this.durationOfLoan = durationOfLoan;
+	}
+
+	public String getLoanId() {
+		return loanId;
+	}
+
+	public void setLoanId(String loanId) {
+		this.loanId = loanId;
+	}
+
+	public String getLoanAccountNumber() {
+		return loanAccountNumber;
+	}
+
+	public void setLoanAccountNumber(String loanAccountNumber) {
+		this.loanAccountNumber = loanAccountNumber;
+	}
+
+	public String getLoanEmi() {
+		return loanEmi;
+	}
+
+	public void setLoanEmi(String loanEmi) {
+		this.loanEmi = loanEmi;
+	}
+
+	public String getIncome() {
+		return income;
+	}
+
+	public void setIncome(String income) {
+		this.income = income;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesign() {
+		return design;
+	}
+
+	public void setDesign(String design) {
+		this.design = design;
+	}
+
+	public String getTexperience() {
+		return texperience;
+	}
+
+	public void setTexperience(String texperience) {
+		this.texperience = texperience;
+	}
+
+	public String getExpwithcompany() {
+		return expwithcompany;
+	}
+
+	public void setExpwithcompany(String expwithcompany) {
+		this.expwithcompany = expwithcompany;
+	}
+
+	public String getLoanStatus() {
+		return loanStatus;
+	}
+
+	public void setLoanStatus(String loanStatus) {
+		this.loanStatus = loanStatus;
+	}
+
+	public String getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
+
+}
