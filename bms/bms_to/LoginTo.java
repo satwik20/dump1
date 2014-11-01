90450d89a09d48619136840c1a57ae0a2d3fdc31
satwik20 <satwik20@users.noreply.github.com>
2014-11-01 16:40:03 -0400

    Create LoginTo.java

---
+++ bms/bms_to/LoginTo.java
@@ -0,0 +1,23 @@
+package com.cts.bms_to;
+
+public class LoginTo {
+
+	public String userName;
+	public String password;
+
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+	
+}
