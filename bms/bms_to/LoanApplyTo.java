1f1b3e82e4f0e77e09638be39ccc36688b6730b5
satwik20 <satwik20@users.noreply.github.com>
2014-11-01 16:39:30 -0400

    Create LoanApplyTo.java

---
+++ bms/bms_to/LoanApplyTo.java
@@ -0,0 +1,137 @@
+package com.cts.bms_to;
+
+public class LoanApplyTo {
+
+	/* Variables for Loan Apply */
+
+	String loanType;
+	String loanAmount;
+
+	String aday;
+	String amonth;
+	String ayear;
+
+	String iday;
+	String imonth;
+	String iyear;
+
+	String rateOfInterest;
+	String durationOfLoan;
+
+	String loanId;
+	String loanAccountNumber;
+	String loanEmi;
+
+	/* Getters and setters for Loan Apply */
+
+	public String getAday() {
+		return aday;
+	}
+
+	public void setAday(String aday) {
+		this.aday = aday;
+	}
+
+	public String getAmonth() {
+		return amonth;
+	}
+
+	public void setAmonth(String amonth) {
+		this.amonth = amonth;
+	}
+
+	public String getAyear() {
+		return ayear;
+	}
+
+	public void setAyear(String ayear) {
+		this.ayear = ayear;
+	}
+
+	public String getIday() {
+		return iday;
+	}
+
+	public void setIday(String iday) {
+		this.iday = iday;
+	}
+
+	public String getImonth() {
+		return imonth;
+	}
+
+	public void setImonth(String imonth) {
+		this.imonth = imonth;
+	}
+
+	public String getIyear() {
+		return iyear;
+	}
+
+	public void setIyear(String iyear) {
+		this.iyear = iyear;
+	}
+
+	public String getDurationOfLoan() {
+		return durationOfLoan;
+	}
+
+	public void setDurationOfLoan(String durationOfLoan) {
+		this.durationOfLoan = durationOfLoan;
+	}
+
+	public String getLoanType() {
+		return loanType;
+	}
+
+	public void setLoanType(String loanType) {
+		this.loanType = loanType;
+	}
+
+	public String getLoanAmount() {
+		return loanAmount;
+	}
+
+	public void setLoanAmount(String loanAmount) {
+		this.loanAmount = loanAmount;
+	}
+
+	/*
+	 * public String getApplyDate() { return applyDate; } public void
+	 * setApplyDate(String applyDate) { this.applyDate = applyDate; } public
+	 * String getIssueDate() { return issueDate; } public void
+	 * setIssueDate(String issueDate) { this.issueDate = issueDate; }
+	 */
+	public String getRateOfInterest() {
+		return rateOfInterest;
+	}
+
+	public void setRateOfInterest(String rateOfInterest) {
+		this.rateOfInterest = rateOfInterest;
+	}
+
+	public String getLoanId() {
+		return loanId;
+	}
+
+	public void setLoanId(String loanId) {
+		this.loanId = loanId;
+	}
+
+	public String getLoanAccountNumber() {
+		return loanAccountNumber;
+	}
+
+	public void setLoanAccountNumber(String loanAccountNumber) {
+		this.loanAccountNumber = loanAccountNumber;
+	}
+
+	public String getLoanEmi() {
+		return loanEmi;
+	}
+
+	public void setLoanEmi(String loanEmi) {
+		this.loanEmi = loanEmi;
+	}
+
+}
