a79d87e232a880cc894dec37aacf80130c110887
satwik20 <satwik20@users.noreply.github.com>
2014-11-01 16:41:04 -0400

    Create RegistrationTo.java

---
+++ bms/bms_to/RegistrationTo.java
@@ -0,0 +1,255 @@
+package com.cts.bms_to;
+
+public class RegistrationTo {
+
+	public String name;
+	public String user_Name;
+	public String password;
+	public String re_EnterPassword;
+	public String gardian_Type;
+	public String guardian_Name;
+	public String adderss;
+	public String citizenship;
+	public String country;
+	public String state;
+	public String email;
+	public String gender;
+	public String marital_Status;
+	public String contact_No;
+	public String dob;
+	public String registrateion_date;
+	public String account_Type;
+	public String bank;
+	public String branch;
+	public String ifsc_code;
+	public String accountNo;
+	public String initial_Amount;
+	public String amount;
+	public String total_amount;
+	public String emi;
+	public String loan_Status;
+	public String loan_Id;
+	
+	public String getLoan_Id() {
+		return loan_Id;
+	}
+	public void setLoan_Id(String loan_Id) {
+		this.loan_Id = loan_Id;
+	}
+	public String getTotal_amount() {
+		return total_amount;
+	}
+	public void setTotal_amount(String total_amount) {
+		this.total_amount = total_amount;
+	}
+	public String getInitial_Amount() {
+		return initial_Amount;
+	}
+	public void setInitial_Amount(String initial_Amount) {
+		this.initial_Amount = initial_Amount;
+	}
+	public String getAmount() {
+		return amount;
+	}
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+	public String getIfsc_code() {
+		return ifsc_code;
+	}
+	public void setIfsc_code(String ifsc_code) {
+		this.ifsc_code = ifsc_code;
+	}
+	public String citizen_Status;
+	public String initial_Deposit;
+	public String id_Proof_Type;
+	public String id_Document_No;
+	public String refrence_Account_Holder_Name;
+	public String reference_Account_Holder_No;
+	public String reference_Account_Holder_Address;
+	public String customer_id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUser_Name() {
+		return user_Name;
+	}
+	public void setUser_Name(String user_Name) {
+		this.user_Name = user_Name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRe_EnterPassword() {
+		return re_EnterPassword;
+	}
+	public void setRe_EnterPassword(String re_EnterPassword) {
+		this.re_EnterPassword = re_EnterPassword;
+	}
+	public String getGardian_Type() {
+		return gardian_Type;
+	}
+	public void setGardian_Type(String gardian_Type) {
+		this.gardian_Type = gardian_Type;
+	}
+	public String getGuardian_Name() {
+		return guardian_Name;
+	}
+	public void setGuardian_Name(String guardian_Name) {
+		this.guardian_Name = guardian_Name;
+	}
+	public String getAdderss() {
+		return adderss;
+	}
+	public void setAdderss(String adderss) {
+		this.adderss = adderss;
+	}
+	public String getCitizenship() {
+		return citizenship;
+	}
+	public void setCitizenship(String citizenship) {
+		this.citizenship = citizenship;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getMarital_Status() {
+		return marital_Status;
+	}
+	public void setMarital_Status(String marital_Status) {
+		this.marital_Status = marital_Status;
+	}
+	public String getContact_No() {
+		return contact_No;
+	}
+	public void setContact_No(String contact_No) {
+		this.contact_No = contact_No;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getAccount_Type() {
+		return account_Type;
+	}
+	public void setAccount_Type(String account_Type) {
+		this.account_Type = account_Type;
+	}
+	public String getBank() {
+		return bank;
+	}
+	public void setBank(String bank) {
+		this.bank = bank;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	public String getCitizen_Status() {
+		return citizen_Status;
+	}
+	public void setCitizen_Status(String citizen_Status) {
+		this.citizen_Status = citizen_Status;
+	}
+	public String getInitial_Deposit() {
+		return initial_Deposit;
+	}
+	public void setInitial_Deposit(String initial_Deposit) {
+		this.initial_Deposit = initial_Deposit;
+	}
+	public String getId_Proof_Type() {
+		return id_Proof_Type;
+	}
+	public void setId_Proof_Type(String id_Proof_Type) {
+		this.id_Proof_Type = id_Proof_Type;
+	}
+	public String getId_Document_No() {
+		return id_Document_No;
+	}
+	public void setId_Document_No(String id_Document_No) {
+		this.id_Document_No = id_Document_No;
+	}
+	public String getRefrence_Account_Holder_Name() {
+		return refrence_Account_Holder_Name;
+	}
+	public void setRefrence_Account_Holder_Name(String refrence_Account_Holder_Name) {
+		this.refrence_Account_Holder_Name = refrence_Account_Holder_Name;
+	}
+	public String getReference_Account_Holder_No() {
+		return reference_Account_Holder_No;
+	}
+	public void setReference_Account_Holder_No(String reference_Account_Holder_No) {
+		this.reference_Account_Holder_No = reference_Account_Holder_No;
+	}
+	public String getReference_Account_Holder_Address() {
+		return reference_Account_Holder_Address;
+	}
+	public void setReference_Account_Holder_Address(
+			String reference_Account_Holder_Address) {
+		this.reference_Account_Holder_Address = reference_Account_Holder_Address;
+	}
+	public String getRegistrateion_date() {
+		return registrateion_date;
+	}
+	public void setRegistrateion_date(String registrateion_date) {
+		this.registrateion_date = registrateion_date;
+	}
+	public String getCustomer_id() {
+		return customer_id;
+	}
+	public void setCustomer_id(String customer_id) {
+		this.customer_id = customer_id;
+	}
+	public String getAccountNo() {
+		return accountNo;
+	}
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
+	}
+	public String getEmi() {
+		return emi;
+	}
+	public void setEmi(String emi) {
+		this.emi = emi;
+	}
+	public String getLoan_Status() {
+		return loan_Status;
+	}
+	public void setLoan_Status(String loan_Status) {
+		this.loan_Status = loan_Status;
+	}
+	
+	
+
+	}
