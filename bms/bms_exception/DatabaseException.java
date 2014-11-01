3a19e61d56bc9f5a87a18541ae1ff3d762f7b87e
satwik20 <satwik20@users.noreply.github.com>
2014-11-01 16:38:08 -0400

    Create DatabaseException.java

---
+++ bms/bms_exception/DatabaseException.java
@@ -0,0 +1,11 @@
+package com.cts.bms_exception;
+
+public class DatabaseException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException(String msg)
+	{
+	super(msg);
+	}
+}
