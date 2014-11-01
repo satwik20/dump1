50f060336825c80b4174ec4d8df4c2c50c31bb0f
satwik20 <satwik20@users.noreply.github.com>
2014-11-01 16:42:20 -0400

    Create ViewTransactionDetailTO.java

---
+++ bms/bms_to/ViewTransactionDetailTO.java
@@ -0,0 +1,56 @@
+package com.cts.bms_to;
+
+public class View_TransactionDetailTO {
+	public String accountNo;
+	public String accountName;
+	public String accountType;
+	public String transactionID;
+	public String transactionType;
+	public String transactionDate;
+	public long  amount;
+	
+	
+	public String getAccountNo() {
+		return accountNo;
+	}
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
+	}
+	public String getAccountName() {
+		return accountName;
+	}
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+	public String getAccountType() {
+		return accountType;
+	}
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+	public String getTransactionID() {
+		return transactionID;
+	}
+	public void setTransactionID(String transactionID) {
+		this.transactionID = transactionID;
+	}
+	public String getTransactionType() {
+		return transactionType;
+	}
+	public void setTransactionType(String transactionType) {
+		this.transactionType = transactionType;
+	}
+	public String getTransactionDate() {
+		return transactionDate;
+	}
+	public void setTransactionDate(String transactionDate) {
+		this.transactionDate = transactionDate;
+	}
+	public long getAmount() {
+		return amount;
+	}
+	public void setAmount(long amount) {
+		this.amount = amount;
+	}
+	
+}
