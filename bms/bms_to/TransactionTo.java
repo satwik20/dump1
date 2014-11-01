3feb95d7cc53ef0bb41d962b599f72c0d04bd3ea
satwik20 <satwik20@users.noreply.github.com>
2014-11-01 16:41:43 -0400

    Create TransactionTo.java

---
+++ bms/bms_to/TransactionTo.java
@@ -0,0 +1,72 @@
+package com.cts.bms_to;
+
+public class TransactionTo {
+
+	
+	public String amount;
+	public String account_no;
+	public String account_name;
+	public String account_type;
+	public String transaction_date;
+	public String transaction_type;
+	public String intial_Deposit;
+	public String total_amount;
+	public String country;
+	
+	public String getAmount() {
+		return amount;
+	}
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+	public String getIntial_Deposit() {
+		return intial_Deposit;
+	}
+	public void setIntial_Deposit(String intial_Deposit) {
+		this.intial_Deposit = intial_Deposit;
+	}
+	public String getTotal_amount() {
+		return total_amount;
+	}
+	public void setTotal_amount(String total_amount) {
+		this.total_amount = total_amount;
+	}
+	public String getAccount_no() {
+		return account_no;
+	}
+	public void setAccount_no(String account_no) {
+		this.account_no = account_no;
+	}
+	public String getAccount_name() {
+		return account_name;
+	}
+	public void setAccount_name(String account_name) {
+		this.account_name = account_name;
+	}
+	public String getAccount_type() {
+		return account_type;
+	}
+	public void setAccount_type(String account_type) {
+		this.account_type = account_type;
+	}
+	public String getTransaction_date() {
+		return transaction_date;
+	}
+	public void setTransaction_date(String transaction_date) {
+		this.transaction_date = transaction_date;
+	}
+	public String getTransaction_type() {
+		return transaction_type;
+	}
+	public void setTransaction_type(String transaction_type) {
+		this.transaction_type = transaction_type;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	
+}
